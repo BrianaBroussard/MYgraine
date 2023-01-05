e35a0b23fc89e338d13c21a1092fbff8b6b2b5b5
Briana Broussard <brianajbroussard@gmail.com>
2023-01-05 16:10:03 -0500

    Twilio works need to troubleshoot timezone with daylight savings

---
+++ job.sh
@@ -0,0 +1,12 @@
+
+#activating virtual env
+echo "source /Users/brianabroussard/src/MYgraine_Project/venv/bin/activate"
+
+#sourcing twilio keys in secrets.sh
+echo "source //Users/brianabroussard/src/MYgraine_Project/secrets.sh"
+
+#run twilio program to notify users
+echo "python3 //Users/brianabroussard/src/MYgraine_Project/send_sms.py"
+
+
+#cron tab runs this program daily at midnight 
--- send_sms.py
+++ send_sms.py
@@ -8,8 +8,9 @@ from model import db, connect_to_db, User
 
 account_sid = os.environ['account_sid']
 auth_token = os.environ['auth_token']
-message_service_sid = os.environ['message_service_sid']
+
 twilio_number = os.environ['twilio_number']
+
 
 
 with app.app_context():
@@ -18,14 +19,15 @@ with app.app_context():
    #Returns list of tuples with all user's name and phone number
      
     users_phone_scheduled_reminder = []
-    query = User.query.filter(User.email == "+19498387000").all() #test query
+    query = User.query.filter(User.phone_number == "+19498387000", User.user_id == 5).all() #test query change id for test user prn 
     # query = User.query.all()
-
+    
+    
     for user in query:
         if user.phone_number and user.scheduled_reminder:
             users_phone_scheduled_reminder.append((user.name, user.phone_number, user.scheduled_reminder))
     
-
+   
     for users in users_phone_scheduled_reminder: 
         
 
@@ -34,16 +36,18 @@ with app.app_context():
         #running program at midnight with cronjob 
         #and did not allow users to schedule between 12:00am and 1:01 to get around this 
         
-        send_when = date.today().isoformat() + users[2].isoformat() #+ '-08:00' #formats time and adjusts for local timezone with -08:00 
+        send_when = date.today().isoformat() + "T" + users[2].isoformat() + "-08:00" #formats time and adjusts for local timezone with -08:00 
         
+        
+   
+        message_service_sid = os.environ['message_service_sid']
 
         message = client.messages.create(
                 from_= message_service_sid,
                 to=users[1],
                 body=f'Hi {users[0]}, did you have a headache today. You should log it in MYgraine!',
                 schedule_type='fixed',
-                send_at=send_when.isoformat()
-                
+                send_at=send_when,
             )
 
         print(message.sid)
